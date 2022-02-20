2da45499c871f32e2ace3b2659d0f785268b5945
dsanfratello <d.sanfratello@studenti.unipi.it>
2022-02-20 16:01:22 +0100

    Deleter old concept for sigma_over_error research

--- starcluster/skew_plots/__init__.py
+++ starcluster/skew_plots/__init__.py
@@ -1,6 +1,1 @@
-from .const import R_GAL, R_S_SAGA
-from .const import PAR_MIN, PAR_MAX, PAR_ERR_MIN, PAR_ERR_MAX
-from .const import GAL_LONG_MIN, GAL_LONG_MAX
-# from skew_distribution import SkewDistribution
-from .parallax_limit import ParallaxLimit
 from .skew_plots import SkewPlots
