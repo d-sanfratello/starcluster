b949382f51526c0a14b7b41c3f0831ccc14b753d
dsanfratello <d.sanfratello@studenti.unipi.it>
2022-02-20 15:59:51 +0100

    Completed plots for mu_over_error

--- starcluster/skew_plots/skew_plots.py
+++ starcluster/skew_plots/skew_plots.py
@@ -5,50 +5,56 @@ from scipy.special import erf
 
 
 class SkewPlots:
-    def plot_fixed_relative(self, mu_over_err=10, L=1e2, N=20):
-        mu = np.linspace(1e-5, 1, N)
-        sigma = mu / mu_over_err
+    def plot_fixed_relative(self, rel_error=0.10, L=50, N=20,
+                            min_parallax=1e-1, max_parallax=10,
+                            sigma_lim=None):
+        mu = np.logspace(np.log10(min_parallax), np.log10(max_parallax), N)
+        sigma = mu * rel_error
 
-        r = np.linspace(1e-10, L, 1000)
+        if sigma_lim is not None:
+            sigma = np.where(sigma <= sigma_lim, sigma, sigma_lim)
+
+        r = np.linspace(1e-5, L, 1000)
 
         fig = plt.figure()
         ax = fig.gca()
         ax.set_title(f'Test with fixed relative error at '
-                     f'{1 / mu_over_err:.3f} (normalized)')
+                     f'{rel_error:.3f} (normalized)\n'
+                     f'Sigma lim set to {sigma_lim}')
         ax.grid()
         for m, s in zip(mu[:10], sigma[:10]):
             distr = self.__distribution(r, m, s, L)
             ax.plot(r, distr/max(distr), linestyle='-',
-                    label=f'mu = {m:.3f}')
+                    label=f'mu = {m:.3f} +- {s:.1f} mas')
         for m, s in zip(mu[10:], sigma[10:]):
             distr = self.__distribution(r, m, s, L)
             ax.plot(r, distr/max(distr), linestyle='--',
-                    label=f'mu = {m:.3f}')
+                    label=f'mu = {m:.3f} +- {s:.1f} mas')
 
+        ax.set_xlabel('$r$ [kpc]')
         ax.legend(loc='best')
-        plt.show()
 
-    def plot_fixed_loc(self, mu=1e-3, L=1e2, N=20):
-        mu_over_err = np.linspace(1e-4, 0.5, N)
+    def plot_fixed_loc(self, mu=5, L=50, N=20):
+        mu_over_err = np.linspace(2, 1e3, N)
         sigma = mu / mu_over_err
 
-        r = np.linspace(1e-10, L, 1000)
+        r = np.linspace(1e-5, L, 1000)
 
         fig = plt.figure()
         ax = fig.gca()
-        ax.set_title(f'Test with fixed mu at {mu:.3f} (normalized)')
+        ax.set_title(f'Test with fixed mu at {mu:.3f} mas (normalized)')
         ax.grid()
         for m, s in zip(mu_over_err[:10], sigma[:10]):
-            distr = self.__distribution(r, m, s, L)
+            distr = self.__distribution(r, mu, s, L)
             ax.plot(r, distr/max(distr), linestyle='-',
-                    label=f'rel_sigma = {1/m:.3f}')
+                    label=f'rel_sigma = {1/m:.2e}')
         for m, s in zip(mu_over_err[10:], sigma[10:]):
-            distr = self.__distribution(r, m, s, L)
+            distr = self.__distribution(r, mu, s, L)
             ax.plot(r, distr/max(distr), linestyle='--',
-                    label=f'rel_sigma = {1/m:.3f}')
+                    label=f'rel_sigma = {1/m:.2e}')
 
+        ax.set_xlabel('$r$ [kpc]')
         ax.legend(loc='best')
-        plt.show()
 
     @staticmethod
     def __distribution(r, mu, sigma, L=1e2):
