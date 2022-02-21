5db9e0ca0361a7bccba41acb94f766bb241bc673
dsanfratello <d.sanfratello@studenti.unipi.it>
2022-02-21 18:09:48 +0100

    added volume_sector derivation, when cone aperture is larger than 1 deg (subject to change). If aperture of sector is 180deg, it will return volume of sphere

--- starcluster/volume.py
+++ starcluster/volume.py
@@ -15,11 +15,8 @@ def volume_cone(data, aperture):
 
     """
     aperture_deg = aperture / 60
-
-    if aperture_deg >= 90:
-        pi_angle_arcmin = 180 * 60
-        return volume_sphere(data) - volume_cone(data,
-                                                 pi_angle_arcmin - aperture)
+    if aperture_deg >= 1:
+        return volume_sector(data, aperture)
 
     aperture = np.deg2rad(aperture_deg)
 
@@ -30,6 +27,24 @@ def volume_cone(data, aperture):
     logvol = np.log(np.pi / 3) + \
         3 * np.log(distances.max()) +\
         2 * np.log(np.tan(aperture))
+
+    return np.exp(logvol)
+
+
+def volume_sector(data, aperture):
+    aperture_deg = aperture / 60
+    if aperture_deg == 180:
+        return volume_sphere(data)
+
+    aperture = np.deg2rad(aperture_deg)
+
+    distances = np.sqrt((data['x'][:] ** 2 +
+                         data['y'][:] ** 2 +
+                         data['z'][:] ** 2))
+
+    logvol = np.log(2 * np.pi / 3) + \
+             3 * np.log(distances.max()) + \
+             np.log(1 - np.cos(aperture))
 
     return np.exp(logvol)
 
