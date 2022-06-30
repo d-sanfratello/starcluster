13c44693b50da1e1075459de84d740d28802d59e
dsanfratello <d.sanfratello@studenti.unipi.it>
2022-06-30 18:40:10 +0200

    Partially added parallax zero point correction.

--- gaia_corr_data.py
+++ gaia_corr_data.py
@@ -1,9 +1,11 @@
 import numpy as np
 import optparse as op
+import pandas as pd
+
 from scipy.stats import multivariate_normal as mn
 from pathlib import Path
 from tqdm import tqdm
-import pandas as pd
+from zero_point import zpt
 
 '''
 ['source_id', 'ra', 'ra_error', 'dec', 'dec_error', 'parallax',
@@ -15,16 +17,19 @@ import pandas as pd
        'ecl_lat']
 '''
 
-def fill_matrix(dra, ddec, dplx, dpmra, dpmdec, dvrad, dradec, draplx, drapmra, drapmdec, ddecplx, ddecpmra, ddecpmdec, dplxpmra, dplxpmdec, dpmrapmdec):
+
+def fill_matrix(dra, ddec, dplx, dpmra, dpmdec, dvrad, dradec,
+                draplx, drapmra, drapmdec, ddecplx, ddecpmra, ddecpmdec, 
+                dplxpmra, dplxpmdec, dpmrapmdec):
 
     errors = np.array([dra, ddec, dplx, dpmra, dpmdec, dvrad])
     
-    row1 = np.array([ 0.5, dradec, draplx, drapmra, drapmdec, 0])*errors * dra
-    row2 = np.array([ 0, 0.5, ddecplx, ddecpmra, ddecpmdec, 0])*errors * ddec
-    row3 = np.array([ 0, 0, 0.5, dplxpmra, dplxpmdec, 0])*errors * dplx
-    row4 = np.array([ 0, 0, 0, 0.5, dpmrapmdec, 0])*errors * dpmra
-    row5 = np.array([ 0, 0, 0, 0,  0.5, 0])*errors * dpmdec
-    row6 = np.array([ 0, 0, 0, 0, 0, 0.5])*errors * dvrad
+    row1 = np.array([0.5, dradec, draplx, drapmra, drapmdec, 0])*errors * dra
+    row2 = np.array([0, 0.5, ddecplx, ddecpmra, ddecpmdec, 0])*errors * ddec
+    row3 = np.array([0, 0, 0.5, dplxpmra, dplxpmdec, 0])*errors * dplx
+    row4 = np.array([0, 0, 0, 0.5, dpmrapmdec, 0])*errors * dpmra
+    row5 = np.array([0, 0, 0, 0,  0.5, 0])*errors * dpmdec
+    row6 = np.array([0, 0, 0, 0, 0, 0.5])*errors * dvrad
     
     cov_mat = np.array([row1, row2, row3, row4, row5, row6])
     cov_mat = cov_mat + cov_mat.T
@@ -33,21 +38,45 @@ def fill_matrix(dra, ddec, dplx, dpmra, dpmdec, dvrad, dradec, draplx, drapmra, 
     
 
 def read_data(file):
-    names = ['ra_error', 'dec_error', 'parallax_error', 'pmra_error','pmdec_error', 'radial_velocity_error', 'ra_dec_corr', 'ra_parallax_corr', 'ra_pmra_corr','ra_pmdec_corr', 'dec_parallax_corr', 'dec_pmra_corr', 'dec_pmdec_corr', 'parallax_pmra_corr', 'parallax_pmdec_corr', 'pmra_pmdec_corr']
+    names = ['ra_error', 'dec_error', 'parallax_error', 'pmra_error', 
+             'pmdec_error', 'radial_velocity_error', 'ra_dec_corr', 
+             'ra_parallax_corr', 'ra_pmra_corr', 'ra_pmdec_corr', 
+             'dec_parallax_corr', 'dec_pmra_corr', 'dec_pmdec_corr', 
+             'parallax_pmra_corr', 'parallax_pmdec_corr', 'pmra_pmdec_corr']
+    # fixme: phot_g_mean_mag, nu_eff_used_in_astrometry,
+    #        pseudocolour, ecl_lat, astrometric_params_solved
     df = pd.read_csv(file)
-    means = np.array([df['ra'],df['dec'],df['parallax'],df['pmra'],df['pmdec'],df['radial_velocity']]).T
-    covs  = np.array([fill_matrix(*[df[name][i] for name in names]) for i in range(len(means))])
+
+    # Parallax zero point correction
+    zpt.load_tables()
+    zero_point = df.apply(zpt.zpt_wrapper, axis=1)
+
+    means = np.array([df['ra'],
+                      df['dec'],
+                      df['parallax'] - zero_point,
+                      df['pmra'],
+                      df['pmdec'],
+                      df['radial_velocity']]).T
+    covs = np.array([
+        fill_matrix(*[df[name][i] for name in names]
+                    ) for i in range(len(means))])
+
     
-    print([df['ra'].max(),df['dec'].max(),df['parallax'].max(),df['pmra'].max(),df['pmdec'].max(),df['radial_velocity'].max()])
-    print([df['ra'].min(),df['dec'].min(),df['parallax'].min(),df['pmra'].min(),df['pmdec'].min(),df['radial_velocity'].min()])
+    print([df['ra'].max(), df['dec'].max(), df['parallax'].max(),
+           df['pmra'].max(), df['pmdec'].max(), df['radial_velocity'].max()])
+    print([df['ra'].min(), df['dec'].min(), df['parallax'].min(),
+           df['pmra'].min(), df['pmdec'].min(), df['radial_velocity'].min()])
     return means, covs
-    
+
+
 def main():
-    
     parser = op.OptionParser()
-    parser.add_option("-i", "--input", type = "string", dest = "input_folder", help = ".csv from GAIA query")
-    parser.add_option("-o", "--output", type = "string", dest = "output_folder", help = "Output folder", default = '.')
-    parser.add_option("--n_samples", type = "int", dest = "n_samples", help = "Number of samples for each star", default = 1000)
+    parser.add_option("-i", "--input", type="string",
+                      dest="input_folder", help=".csv from GAIA query")
+    parser.add_option("-o", "--output", type="string",
+                      dest="output_folder", help="Output folder", default='.')
+    parser.add_option("--n_samples", type="int", dest="n_samples",
+                      help="Number of samples for each star", default=1000)
 
     (options, args) = parser.parse_args()
     
@@ -59,9 +88,11 @@ def main():
     means, covs = read_data(options.input_folder)
     np.savetxt('means.txt', means)
 
-    for i, (m, s) in tqdm(enumerate(zip(means, covs)), total = len(means), desc = 'Stars'):
+    for i, (m, s) in tqdm(enumerate(zip(means, covs)),
+                          total=len(means), desc='Stars'):
         samples = mn(m, s).rvs(options.n_samples)
         np.savetxt(Path(options.output_folder, '{0}.txt'.format(i)), samples)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
