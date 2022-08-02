8855d7c1e769feb99d2c8e623188484528c3629d
dsanfratello <d.sanfratello@studenti.unipi.it>
2022-08-02 15:00:56 +0200

    Moved A_G_INV transformation matrix form utils subpackage to const module

--- starcluster/const.py
+++ starcluster/const.py
@@ -6,3 +6,11 @@ PC2KM = (648000/np.pi) * 149597870.700  # km
 PC2KPC = 1e-3
 # Conversion constant from julian yr to seconds.
 YR2S = 365.25 * 86400  # s
+
+# The transformation matrix from ICRS coordinates to galactic coordinates,
+# named as the inverse matrix from galactic to ICRS coordinates. The exact
+# values are as defined in Hobbs et al., 2021, Ch.4.
+A_G_INV = np.array([
+    [-0.0548755604162154, -0.8734370902348850, -0.4838350155487132],
+    [0.4941094278755837, -0.4448296299600112, 0.7469822444972189],
+    [-0.8676661490190047, -0.1980763734312015, 0.4559837761750669]])
